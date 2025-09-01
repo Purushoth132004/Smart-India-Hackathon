ecaa532b178957fb683e375622a96d52bb3472c4
Purushoth132004 <mpurushoth475@gmail.com>
2025-09-01 23:25:40 +0530

    frontend-folder-structure-added

---
+++ Frontend-SIH/src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header.jsx"
+import Card from "./components/Card.jsx";
+import "./styles/Header.css"
+function App() {
+  return (
+   <></> );
+}
+
+export default App;
