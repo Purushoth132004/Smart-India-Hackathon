3c481b76e461302baddaa019e5ccab48d4beba2c
Purushoth132004 <mpurushoth475@gmail.com>
2025-09-05 15:03:43 +0530

    prototype

---
+++ Frontend-SIH/src/pages/Collegecards.jsx
@@ -0,0 +1,13 @@
+function MedicalCollege(props){
+    return(
+      <div>
+        <img src="" alt="" />
+        <div>
+          Modern Medicine
+        </div>
+        
+      </div>
+    );
+}
+
+export default MedicalCollege;
